255dbd1c1555c52cf6de69cc19993c000a989235
briggsy007 <hunter.briggs@outlook.com>
2021-07-21 16:18:14 -0400

    Testing out tip

--- orion/src/App.js
+++ orion/src/App.js
@@ -1,36 +1,52 @@
 import './App.css';
 import Footer from '../src/Components/Footer';
-import Input from './Components/Input'
 import React, { Component } from 'react'
 
 export default class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      billAmount: 0.0,
-      tipPercentage: 0.0
-    }
 
-    this.submit = this.submit.bind(this);
+  handleTotal = (event) => {
+    this.setState({
+      total: event.target.value
+    })
+  }
+  handleTip = (event) => {
+    this.setState({
+      tipPercentage: event.target.value
+    })
   }
 
-  submit() {
-    
+  calcTip = (event) => {
+    event.preventDefault();
+    let tip = parseFloat(this.state.total * this.state.tipPercentage / 100).toFixed(2)
+    this.setState({
+      totalTip: tip
+    })
+
+    console.log(tip + " " + this.state.totalTip)
   }
 
+  state = {
+    total: "",
+    tipPercentage: "",
+    totalTip: ""
+  }
 
 
   render() {
     return (
       <div className = "App">
-        <form>
+        <form onSubmit={this.calcTip}>
+            <div>
+              <h1>Ally Tip Calulator</h1>
+            </div>
             <label>Bill Amount:</label>
-            <input type="number" name="amount" id="bill"></input>
+            <input type="text" value = {this.state.total} onChange= {this.handleTotal} name="amount" id="bill"></input>
             <br></br>
             <label>Tip Percentage:</label>
-            <input type="number" name="tip" id="tip"></input>
+            <input type="text" value = {this.state.tipPercentage} onChange={this.handleTip} name="tip" id="tip"></input>
             <input type="submit" value="Calculate Tip" ></input>
-          </form>
+        </form>
+        <p>tip: ${this.state.totalTip}</p>
 
 
         <Footer/>
