7b83c860a8d0369de1a0fc4f53b97faee832c4b5
John Won <johnjywon@gmail.com>
2021-07-22 12:24:53 -0400

    Adding component, Logic works, Math works, Output works

--- orion/src/App.js
+++ orion/src/App.js
@@ -1,32 +1,30 @@
-import './App.css';
-import Footer from '../src/Components/Footer';
-import Header from './Components/Header'
-import TipCalculator from './Components/TipCalculator';
-import React, { Component } from 'react'
+import "./App.css";
+import Footer from "../src/Components/Footer";
+import Header from "./Components/Header";
+import TipCalculator from "./Components/TipCalculator";
+import React, { Component } from "react";
+import Interest from "./Components/Interest";
 
 export default class App extends Component {
   state = {
-    page: "TipCalculator"
-  }
-
+    page: "TipCalculator",
+  };
 
   render() {
-
     const page = this.state.page;
 
     let calc;
     if (page === "TipCalculator") {
-      calc = <TipCalculator />
+      calc = <TipCalculator />;
     }
 
     return (
-      <div className = "App">
+      <div className="App">
         <Header />
-        {calc}
-        <Footer/>
+        <Interest />
+        {/* {calc} */}
+        <Footer />
       </div>
     );
   }
 }
-
-
