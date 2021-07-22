17386cc8c55885a0d48df9db0209f4e77d0c43f9
John Won <johnjywon@gmail.com>
2021-07-22 09:46:53 -0400

    Submit Bill function works, Updated state and return updated state

--- orion/src/App.js
+++ orion/src/App.js
@@ -1,31 +1,40 @@
-import './App.css';
-import Footer from '../src/Components/Footer';
-import Input from './Components/Input'
-import React, { Component } from 'react'
+import "./App.css";
+import Footer from "../src/Components/Footer";
+import Input from "./Components/Input";
+import React, { Component } from "react";
 
 export default class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       billAmount: 0.0,
-      tipPercentage: 0.0
-    }
-
-    this.submit = this.submit.bind(this);
+      tipPercentage: 0.0,
+      totalTip: 0.0,
+      totalBill: 0.0,
+    };
   }
-
-
-  submit() {
-    
-  }
-
-
+  submitBill = (bill, tip) => {
+    // console.log(bill, " line 17");
+    // console.log(tip, " line 20");
+    const total = bill * (tip / 100).toFixed(2);
+    // console.log(total, "this is total");
+    const totalBill = total + parseInt(bill);
+    // console.log(totalBill, "total bill here");
+    this.setState({
+      tipPercentage: bill,
+      billAmount: bill,
+      totalTip: total,
+      totalBill: totalBill,
+    });
+  };
 
   render() {
     return (
-      <div className = "App">
-        <Input />
-        <Footer/>
+      <div className="App">
+        <Input submit={this.submitBill} />
+        <h2>Tip Amount: {this.state.totalTip}</h2>
+        <h2>Total Bill: {this.state.totalBill}</h2>
+        <Footer />
       </div>
     );
   }
--- orion/src/Components/Input.js
+++ orion/src/Components/Input.js
@@ -1,17 +1,33 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 export default function Input(props) {
-    return (
-        <div>
-            <form>
-                <label>Bill Amount:</label>
-                <input type="text" name="amount"></input>
-                <br></br>
-                <label>Tip Percentage:</label>
-                <input type="text" name="tip"></input>
-            </form>
+  const [bill, setBill] = useState("");
+  const [tip, setTip] = useState("");
 
-            <button onClick={props.submit}>Submit</button>
-        </div> 
-    )
+  //handler for the getting the bill from input
+  const handleGetBill = (event) => {
+    setBill(event.target.value);
+  };
+  //handler for the getting the tip from input
+  const handleGetTip = (event) => {
+    setTip(event.target.value);
+  };
+
+  const submitBill = () => {
+    props.submit(bill, tip);
+  };
+  return (
+    <div>
+      <form>
+        <label>Bill Amount:</label>
+        <input type="text" onChange={handleGetBill} name="amount"></input>
+        <br></br>
+        <label>Tip Percentage:</label>
+        <input type="percentage" onChange={handleGetTip} name="tip"></input>
+      </form>
+
+      <button onClick={submitBill}>Submit</button>
+    </div>
+  );
 }
