5d2c3e1cbb27600be93c06da38d6ef2530200c27
briggsy007 <hunter.briggs@outlook.com>
2021-07-22 09:31:33 -0400

    Fixed calc and suggested

--- orion/src/App.js
+++ orion/src/App.js
@@ -18,17 +18,18 @@ export default class App extends Component {
   calcTip = (event) => {
     event.preventDefault();
     let tip = parseFloat(this.state.total * this.state.tipPercentage / 100).toFixed(2)
+    let bill = parseFloat(parseFloat(this.state.total) + parseFloat(tip)).toFixed(2)
     this.setState({
-      totalTip: tip
+      totalTip: tip,
+      totalBill: bill
     })
-
-    console.log(tip + " " + this.state.totalTip)
   }
 
   state = {
     total: "",
     tipPercentage: "",
-    totalTip: ""
+    totalTip: "",
+    totalBill: ""
   }
 
 
@@ -47,6 +48,14 @@ export default class App extends Component {
             <input type="submit" value="Calculate Tip" ></input>
         </form>
         <p>tip: ${this.state.totalTip}</p>
+        <p>total bill: ${this.state.totalBill}</p>
+        <div>
+            <h2>Suggested Tips</h2>
+            <h3>10%: ${this.state.total * .10}</h3>
+            <h3>15%: ${this.state.total * .15}</h3>
+            <h3>20%: ${this.state.total * .2}</h3>
+            <h3>25%: ${this.state.total * .25}</h3>
+        </div>
 
 
         <Footer/>
